31b3ae638ff67b095077efc62842e1cbad169e62
Maximilian Lucuta <113853857+maxlucuta@users.noreply.github.com>
2023-06-07 21:24:28 +0000

    fixed usage

---
+++ makefile
@@ -0,0 +1,2 @@
+main: usage.cpp
+	g++ -g -Wall -std=c++2a usage.cpp -o main
--- usage.cpp
+++ usage.cpp
@@ -10,14 +10,35 @@ int sub(int a, int b) {
     return a - b; 
 }
 
+class Demo {
+    private:
+        const char* name;
+    
+    public:
+        Demo(const char* name) : name(name) {}
+        void foo() { std::cout << "Foo: " << name << std::endl; }
+        void bar() { std::cout << "Bar: " << name << std::endl; }
+};
+
 
 int main() {
 
     BranchChanger branch = BranchChanger(add, sub);
-    branch.set_direction(true);
+    branch.setDirection(true);
     std::cout << branch.branch(2,3) << std::endl;
-    branch.set_direction(false);
+    branch.setDirection(false);
     std::cout << branch.branch(2,3) << std::endl;
+
+    Demo demoObject1("Imperial");
+    Demo demoObject2("UCL");
+    BranchChangerClass branchClass(&Demo::foo, &Demo::bar);
+    branchClass.setDirection(true);
+    branchClass.branch(demoObject1);
+    branchClass.branch(demoObject2);
+    branch.setDirection(false);
+    branchClass.branch(demoObject1);
+    branchClass.branch(demoObject2);
+
     return 0;
 
 }
