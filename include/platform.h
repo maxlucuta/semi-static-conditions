4b7ee1168155460cc1063be0e842964458c4aadd
Maximilian Lucuta <max.lucuta22@imperial.ac.uk>
2023-08-07 17:08:26 +0100

    ARM

--- include/platform.h
+++ include/platform.h
@@ -6,11 +6,14 @@
 #define SEMI_STATIC_CONDITIONS_PLATFORM_H
 
 
-#ifdef __x86_64__ 
-#define JUMP_OPCODE_ 0xe9
+#if defined(__x86_64__) 
+    #define JUMP_OPCODE_ 0xE9
+    #define JMP_DISTANCE_ 2e32
+#elif defined(__arm__)
+    #define JUMP_OPCODE_ 0xEA
+    #define JMP_DISTANCE_ 2e24
 #endif
 
-#define JMP_DISTANCE_ 2e32
 #define DWORD_ 4
 
 #ifndef __cplusplus
@@ -20,36 +23,52 @@
 #endif
 
 #if defined(_WIN32)
-#define PLATFORM_WINDOWS
-#include <Windows.h>
+    #define PLATFORM_WINDOWS
+    #include <Windows.h>
 #elif defined(__linux__)
-#define PLATFORM_LINUX
-#include <sys/mman.h>
-#include <unistd.h>
+    #define PLATFORM_LINUX
+    #include <sys/mman.h>
+    #include <unistd.h>
 #elif defined(__APPLE__)
+    #include <sys/mman.h>
+    #include <unistd.h>
     #define PLATFORM_MAC
 #else
     #error "Platform not supported!"
 #endif
 
 
-#if defined(__clang__) || defined(__GNUC__)
-    #define ASM_INLINE asm("jmp 0x0");
+
+#if (defined(__clang__) || defined(__GNUC__))
+
+    #if defined(__x86_64__)
+        #define ASM_INLINE asm("jmp 0x0");
+    #else
+        #define ASM_INLINE asm("b 0x0");
+    #endif
+
     #ifdef __has_attribute
         #if __has_attribute(optimize)
+
             #define USING_GCC
             #if __has_attribute(nocf_check)
                 #define ATTRIBUTES __attribute__((hot,nocf_check,optimize("no-ipa-cp-clone,O3")))
             #else
                 #define ATTRIBUTES __attribute__((hot,optimize("no-ipa-cp-clone,O3")))
             #endif
+
         #else
             #define USING_CLANG
             #define ATTRIBUTES __attribute__((hot,noinline))
         #endif
     #endif
+
 #elif defined(_MSC_VER)
-    #define ASM_INLINE __asm{ jmp $+0x0 };
+    #if defined(__x86_64__)
+        #define ASM_INLINE __asm{ jmp $+0x0 };
+    #else
+        #define ASM_INLINE
+    #endif
     #define USING_MSVC
     #define ATTR __delspec(noinline)
 #else
