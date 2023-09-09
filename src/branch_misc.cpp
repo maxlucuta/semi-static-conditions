2e2217933ca6e5b38ce83c2d5ad8f80e3f721245
Maximilian Lucuta <113853857+maxlucuta@users.noreply.github.com>
2023-09-09 12:55:36 +0100

    Update branch_misc.cpp

--- src/branch_misc.cpp
+++ src/branch_misc.cpp
@@ -8,16 +8,16 @@ std::string err_to_str(const error_codes& code) {
 
         case error_codes::BRANCH_TARGET_OUT_OF_BOUNDS:
 
-            return R"(Supplied branch targets (as function pointers) exceed a 2GiB displacement
-					  from the entry point in the text segment, and cannot be reached with a 32-bit
-					  relative jump. Consider moving the entry point to different areas in the text
-					  segment by altering hot/cold attributes.)";
+            return R"(Supplied branch targets (as function pointers) exceed a 4GiB displacement
+		      from the entry point in the text segment, and cannot be reached with a 32-bit
+		      relative jump. Consider moving the entry point to different areas in the text
+		      segment by altering hot/cold attributes.)";
 
         case error_codes::MULTIPLE_INSTANCE_ERROR:
 
             return R"(More than once instance for template specialised semi-static conditions detected.
-					  Program terminated as multiple instances sharing the same entry point is dangerous
-					  and results in undefined behaviour (multiple instances write to same function.))";
+		      Program terminated as multiple instances sharing the same entry point is dangerous
+		      and results in undefined behaviour (multiple instances write to same function.))";
 
         case error_codes::PAGE_PERMISSIONS_ERROR:
 
